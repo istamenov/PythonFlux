b07c87c7eb3f092d52c772d3016dd9f402ee606d
istamenov <screwgarena@abv.bg>
2016-05-25 19:29:35 +0300

    server update

--- server.py
+++ server.py
@@ -1,23 +1,22 @@
 import asyncio
 
 class Server(asyncio.Protocol):
-    def __init__(self, loop):
-        self.game = ''.encode()
-        server = loop.create_server(Server, host='localhost', port=11111, reuse_address=True)
-        loop.run_until_complete(server)
+    def __init__(self):
+        self.game = ''
 
     def data_received(self, data):        
         self.transport.write("You:".encode())
-        self.transport.write(turn)
+        self.transport.write(data)
         self.transport.write("Everyone:".encode())
-        self.transport.write(self.game)
-        self.game.append(turn)
+        self.transport.write(self.game.encode())
         self.transport.close()
+        self.game += data.decode()
 
     def connection_made(self, transport):
         self.transport = transport
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    Server(loop)
+    srv = loop.create_server(Server, host='localhost', port=11111, reuse_address=True)
+    loop.run_until_complete(srv)
     loop.run_forever()
